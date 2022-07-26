8a02b6c5565e96b0e5df30e5bf14cc4c673a565e
brandonmitchell217 <brandonmitchell217@gmail.com>
2022-07-26 12:43:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-mz8ues
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-mz8ues)
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Store} from './pages/Store'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      <Route path="/about" element={<About />}>
+      <Route path="/store" element={<Store />}>
+    </Routes>
+    </>
+  )
+}
+
+export default App
