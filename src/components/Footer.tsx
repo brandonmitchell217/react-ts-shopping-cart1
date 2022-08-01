5d9dc5c8d52c30d375a76fb1c9b57009268937a8
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-08-01 13:03:15 -0400

    Footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import { Container, Nav, NavLink } from "react-bootstrap";
+
 export function Footer() {
   return (
-    <footer>
-      <p>The footer</p>
+    <footer className="p-3">
+      <Container className="d-flex justify-content-between align-items-center">
+        <h4>Footer</h4>
+        <Nav className="">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+        </Nav>
+      </Container>
     </footer>
   );
 }
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -19,7 +19,7 @@ export function StoreItem({ id, name, image, price, type }: StoreItemProps) {
         <Card.Img src={image} variant="top" />
       </Link>
       <Card.Body className="px-3">
-        <h3 className="text-center">{name}</h3>
+        <h3>{name}</h3>
         <span>${price}</span>
         <span className="d-block text-muted text-lowercase">{type}</span>
         <Button
