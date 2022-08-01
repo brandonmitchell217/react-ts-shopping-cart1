173a5c18d887ac2ce7e8d06e9efef5d0de5bbe0e
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-08-01 11:40:53 -0400

    Styling for the home shop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,40 @@
 import { Col, Row, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { StoreItem } from "../components/StoreItem";
 
 export function Home({ data }: any) {
   let itemsShown = [];
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= data.length - 4; i++) {
     let [things] = data.sort(() => Math.random() - 0.5).slice(0, 4);
     itemsShown.push(things);
   }
   return (
     <section className="pb-3">
-      <Container>
+      <Container className="d-flex flex-column">
         <h1>Home</h1>
-        <Row md={4} xs={2} lg={4} className="g-3 px-5">
-          {itemsShown.map((item: any) => (
-            <Col>
-              <StoreItem
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                price={item.price}
-                type={item.type}
-              />
-            </Col>
-          ))}
-        </Row>
+        <div className="p-5">
+          <h1>Shop</h1>
+          <Row md={4} xs={2} lg={4} className="g-3 px-5">
+            {itemsShown.map((item: any) => (
+              <Col>
+                <StoreItem
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  price={item.price}
+                  type={item.type}
+                />
+              </Col>
+            ))}
+          </Row>
+        </div>
+        <Link
+          to="/store"
+          className="text-white text-center bg-primary rounded py-2 px-4 mx-auto"
+          style={{ textDecoration: "none", width: "200px" }}
+        >
+          Visit our Shop
+        </Link>
       </Container>
     </section>
   );
